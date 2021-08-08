8015361042426b4e55dddacb1da1b2db08e5e255
Aaron Janes <aaron.janes@gmail.com>
2021-08-08 11:04:21 -0600

    counter practice

--- practice.py
+++ practice.py
@@ -1,5 +1,30 @@
-print("hello")
+print("Hello Aaron ")
 
+# for item in [1,2,3,4,5,6,7]:
+#   print(item)
+  
+user = {
+  "name": "Golem",
+  "age": 5006,
+  "can_swim": False
+}
 
-for item in [1,2,3,4,5,6,7]:
-  print(item)
+# for item in user.items():
+#   print(item)
+  
+  
+
+# for item in user.values():
+#   print(item)
+  
+  
+# for key, value in user.items():
+#   if key == "name":
+#     print(value)
+  
+my_list= [1,2,3,4,5,6,7,8,9]
+
+count = 0 
+for item in my_list:
+  count += item
+  print(count)
