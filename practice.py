5a1b633923098f77db587c1dd4fa4b6261dd7684
Aaron Janes <aaron.janes@gmail.com>
2021-08-08 19:51:10 -0600

    Tesla exercise

--- practice.py
+++ practice.py
@@ -80,9 +80,28 @@ user = {"name": "Golem", "age": 5006, "can_swim": False}
 # print(answer)
 
 
-def say_hello():
-    response = input("Who are you ?")
-    print(f"Hello {response}")
+# def say_hello():
+#     response = input("Who are you ?")
+#     print(f"Hello {response}")
 
 
-say_hello()
+# say_hello()
+def checkDriverAge():
+    age = input("What is your age?: ")
+
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!")
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+
+# 1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age.
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+# 2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+# checkDriverAge(92);
+# it returns "Powering On. Enjoy the ride!"
+# also make it so that the default age is set to 0 if no argument is given.
+checkDriverAge()
