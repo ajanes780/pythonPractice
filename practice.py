42688f7e406a870201008b4fd5b8993d7ad241c4
Aaron Janes <aaron.janes@gmail.com>
2021-08-08 20:25:53 -0600

    Walrus

--- practice.py
+++ practice.py
@@ -86,15 +86,15 @@ user = {"name": "Golem", "age": 5006, "can_swim": False}
 
 
 # say_hello()
-def checkDriverAge():
-    age = input("What is your age?: ")
+# def checkDriverAge():
+#     age = input("What is your age?: ")
 
-    if int(age) < 18:
-        print("Sorry, you are too young to drive this car. Powering off")
-    elif int(age) > 18:
-        print("Powering On. Enjoy the ride!")
-    elif int(age) == 18:
-        print("Congratulations on your first year of driving. Enjoy the ride!")
+#     if int(age) < 18:
+#         print("Sorry, you are too young to drive this car. Powering off")
+#     elif int(age) > 18:
+#         print("Powering On. Enjoy the ride!")
+#     elif int(age) == 18:
+#         print("Congratulations on your first year of driving. Enjoy the ride!")
 
 
 # 1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age.
@@ -104,4 +104,22 @@ def checkDriverAge():
 # checkDriverAge(92);
 # it returns "Powering On. Enjoy the ride!"
 # also make it so that the default age is set to 0 if no argument is given.
-checkDriverAge()
+# checkDriverAge()
+
+
+# def highest_even(li):
+#     evens = []
+#     for item in li:
+#         if item % 2 == 0:
+#             evens.append(item)
+#     return max(evens)
+
+
+# print(highest_even([10, 2, 3, 4, 8, 11]))
+
+# walrus - really cool looks like GOlang
+# :=
+
+a = "hellloooooooooooo"
+if (n := len(a)) > 10:
+    print(f"Too long {n} elements")
