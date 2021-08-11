a9ca7add298054cc6fdf08a60b1d9dd20b1b2f63
Aaron Janes <aaron.janes@gmail.com>
2021-08-11 08:41:43 -0600

    sorting duplicates using list comprehension and sets

--- compreshesions.py
+++ compreshesions.py
@@ -6,5 +6,14 @@ my_list3 = [char ** 2 for char in range(0, 100) if char % 2 == 0]
 # print(my_list3, "*********")
 simple_dict = {"bob": 2, "frank": 3}
 
-my_dict = {key: value ** 2 for key, value in simple_dict.items()}
-print(my_dict)
+my_dict = {key: value ** 2 for key, value in simple_dict.items() if value > 2}
+my_dict2 = {num: num * 2 for num in [1, 2, 3]}
+
+
+some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
+
+
+duplicatics = list(set([char for char in some_list if some_list.count(char) >= 2]))
+
+
+print(duplicatics)
