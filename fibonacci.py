63c70f88a41c02a6c3c9f650fa3df5aea28224bc
Aaron Janes <aaron.janes@gmail.com>
2021-08-13 20:40:54 -0600

    Working on testing

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,16 @@
-def fibonacci_number(number):
-    a = 0
-    b = 1
-    for i in range(number):
-        yield a
-        temp = a
-        a = b
-        b = temp + b
+def fibonacci_number(num):
+
+    if num:
+        a = 0
+        b = 1
+        for i in range(num):
+            yield a
+            temp = a
+            a = b
+            b = temp + b
+    else:
+        return "Please Choose a number greater then 0"
 
 
-# for i in fibonacci_number(21):
-#     print(i)
-
-
-my_list = [i for i in fibonacci_number(21)]
+my_list = [i for i in fibonacci_number(-1)]
 print(my_list)
--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 import unittest
-
+from unittest import result
 import fibonacci
 
 
@@ -8,32 +8,13 @@ class TestMain(unittest.TestCase):
         params = 21
         result = [i for i in fibonacci.fibonacci_number(params)]
 
-        self.assertEqual(
-            result,
-            [
-                0,
-                1,
-                1,
-                2,
-                3,
-                5,
-                8,
-                13,
-                21,
-                34,
-                55,
-                89,
-                144,
-                233,
-                377,
-                610,
-                987,
-                1597,
-                2584,
-                4181,
-                6765,
-            ],
-        )
+        self.assertEqual(result, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765])
+
+    def test_fibonacci2(self):
+        params = -1
+        result = [i for i in fibonacci.fibonacci_number(params)]
+
+        self.assertEqual(result, "Please Choose a number greater then 0")
 
 
 if __name__ == "__main__":
